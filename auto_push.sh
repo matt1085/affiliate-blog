8959ad3425e07ee05542a2214d599d6d8a137264
Elizabeth <matt.peterson1085@gmail.com>
2025-08-31 17:13:12 +0000

    Automated update: 2025-08-31 17:13:12

--- auto_push.sh
+++ auto_push.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# Go to the blog folder
-cd /home/elizabeth/affiliate-blog
+# Full path to Hugo binary
+HUGO_BIN="/snap/bin/hugo"
 
-# Build Hugo site
-hugo
+# Full path to your blog
+BLOG_DIR="/home/elizabeth/affiliate-blog"
 
-# Stage all changes
+# Git settings
+GIT_BRANCH="main"
+GIT_REPO="git@github.com:matt1085/affiliate-blog.git"
+
+# Move to blog directory
+cd $BLOG_DIR || exit
+
+# Build the site
+$HUGO_BIN --minify
+
+# Add changes to git
 git add .
 
-# Commit with a timestamp
-git commit -m "Automated update: $(date '+%Y-%m-%d %H:%M:%S')"
+# Commit with timestamp
+git commit -m "Automated update: $(date '+%Y-%m-%d %H:%M:%S')" 2>/dev/null
 
-# Push to GitHub
-git push origin main
+# Push to remote
+git push $GIT_REPO $GIT_BRANCH
+
